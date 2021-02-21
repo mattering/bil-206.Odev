c517b0b699d3e25e76861ee6f1a3b638068dcbf4
mattering <turdaliev.raatbek@gmail.com>
2021-02-22 03:24:25 +0600

    odev 3

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,142 @@
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    Model model = new Model();
-    View view = new View();
+public class Controller implements ActionListener {
+
+
+    Model model;
+    View view;
+    Controller()
+    {
+        model = new Model();
+        view = new View();
+        view.bStart.addActionListener(this);
+        view.add(view.bStart);
+
+    }
+
 
     public  void execute(){
+
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(view.charCount.isSelected())
+        {
+
+        }
+        else if(view.wordCount.isSelected())
+        {
+            File file = new File(view.textField_1.getText());
+
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+            String st;
+            StringBuilder allText = new StringBuilder("");
+            while (true)
+            {
+                try {
+                    if (!((st = br.readLine()) != null)) break;
+                    else {
+                        allText.append(st);
+                    }
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
+            }
+            String words[] = (allText.toString()).split("\\s");
+
+
+            Arrays.sort(words);
+           // for(int i=0;i< words.length;i++) System.out.print(words[i]+"   ");
+
+
+            Map<String,Integer> map =new HashMap<String,Integer>();
+            int counter=0;
+            int j=0; //j is same checker index
+
+            for(int i=0;i<words.length;i++)
+            {
+                if(words[i].equals(words[j]))
+                {
+                    counter++;
+                }
+                else {
+                    map.put(words[j],counter);
+                   // System.out.println(words[j]+ " : " + counter);
+
+                    counter=0;
+                    j=i;
+                    i=i-1;
+
+                }
+
+            }
+            if(counter!=0)
+            {
+                map.put(words[j],counter);
+               // System.out.println(words[j]+ " : " + counter);
+            }
+
+            if(view.sort_ascending.isSelected())
+            {
+                Map<String,Integer>  sortedMap =  map.entrySet().
+                        stream().
+                        sorted(Map.Entry.comparingByKey()).
+                        collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                view.textArea.setText("");
+                sortedMap.forEach((k, v) -> view.textArea.append((k + ":" + v+"\n")));
+
+            }
+            else if(view.sort_descending.isSelected())
+            {
+                Map<String,Integer>  sortedMapReverseOrder =  map.entrySet().
+                        stream().
+                        sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).
+                        collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                view.textArea.setText("");
+                sortedMapReverseOrder.forEach((k, v) -> view.textArea.append((k + ":" + v + "\n")));
+            }
+            else if(view.sort_frequencyAscending.isSelected())
+            {
+                Map<String,Integer>  sortedMap =  map.entrySet().
+                        stream().
+                        sorted(Map.Entry.comparingByValue()).
+                        collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                view.textArea.setText("");
+                sortedMap.forEach((k, v) -> view.textArea.append((k + ":" + v + "\n")));
+            }
+            else if(view.sort_frequencyDescending.isSelected())
+            {
+                Map<String,Integer>  sortedMapReverseOrder =  map.entrySet().
+                        stream().
+                        sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).
+                        collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+                view.textArea.setText("");
+                sortedMapReverseOrder.forEach((k, v) -> view.textArea.append((k + ":" + v + "\n")));
+
+            }
+
+
+
+
+
+
+        }
 
     }
 }
--- src/View.java
+++ src/View.java
@@ -1,12 +1,14 @@
 import  javax.swing.*;
-import  java.awt.*;
 
 public class View extends  JFrame {
 
-    JRadioButton button_1,button_2, button_3, button_4;
+    JRadioButton charCount, wordCount, button_3, button_4;
+    JRadioButton sort_ascending, sort_descending, sort_frequencyAscending, sort_frequencyDescending;
     JTextField textField_1, textField_2;
     JMenu file,menu;
     JButton bStart;
+    JTextArea textArea;
+    JLabel countShower;
 
 
 
@@ -24,45 +26,80 @@ public class View extends  JFrame {
 
 
 
-        textField_1 = new JTextField("Text File name");
+        textField_1 = new JTextField("test.txt");
         textField_2 = new JTextField("Text Field 2");
 
 
 
 
-        button_1 = new JRadioButton("RButton 1");
-        button_2 = new JRadioButton("RButton 2");
+        charCount = new JRadioButton("Count chars");
+        wordCount = new JRadioButton("Count words");
         button_3 = new JRadioButton("RButton 3");
         button_4 = new JRadioButton("RButton 4");
 
         bStart = new JButton("Start/Go");
 
 
-        textField_1.setBounds(10,20,120,30);
-        button_1.setBounds(   5,50,100,30);
-        button_2.setBounds(   5,80,100,30);
-        button_3.setBounds(   5,110,100,30);
-        button_4.setBounds(   5,140,100,30);
-        textField_2.setBounds(10,170,120,30);
-        bStart.setBounds(20,250,100,40);
+        textField_1.setBounds( 10, 20, 150,30);
+        charCount.setBounds(   5,  50, 150,30);
+        wordCount.setBounds(   5,  80, 150,30);
+        button_3.setBounds(    5,  110,150,30);
+        button_4.setBounds(    5,  140,150,30);
+        textField_2.setBounds( 10, 170,150,30);
+        bStart.setBounds(      20, 250,100,40);
 
 
 
         ButtonGroup bg = new ButtonGroup();
-        bg.add(button_1); bg.add(button_2); bg.add(button_3); bg.add(button_4);
+        bg.add(charCount); bg.add(wordCount); bg.add(button_3); bg.add(button_4);
+
         this.add(textField_1);
-        this.add(button_1);
-        this.add(button_2);
+        this.add(charCount);
+        this.add(wordCount);
         this.add(button_3);
         this.add(button_4);
         this.add(textField_2);
-        this.add(bStart);
+
+
+//        this.add(bStart);
+
+
+        countShower = new JLabel("Total:");
+        countShower.setBounds(175,40,100,30);
+        this.add(countShower);
+
+        sort_ascending = new JRadioButton("Sort by ascending");
+        sort_descending = new JRadioButton("Sort by descending");
+        sort_frequencyAscending = new JRadioButton("Sort by frequency ascending");
+        sort_frequencyDescending = new JRadioButton("Sort by frequency descending");
+
+        sort_ascending.setBounds(          175,280,200,30);
+        sort_descending.setBounds(         175,310,200,30);
+        sort_frequencyAscending.setBounds( 175,340,200,30);
+        sort_frequencyDescending.setBounds(175,370,200,30);
+
+        ButtonGroup sortBg = new ButtonGroup();
+        sortBg.add(sort_ascending); sortBg.add(sort_descending); sortBg.add(sort_frequencyAscending); sortBg.add(sort_frequencyDescending);
+
+        this.add(sort_ascending);
+        this.add(sort_descending);
+        this.add(sort_frequencyAscending);
+        this.add(sort_frequencyDescending);
+
+        textArea = new JTextArea(20, 20);
+        JScrollPane scrollableTextArea = new JScrollPane(textArea);
+
+        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        scrollableTextArea.setBounds(180,70,100,200);
+
+        this.add(scrollableTextArea);
 
 
 
 
-
-        setSize(400,400);
+        setSize(900,600);
         setPreferredSize(getSize());
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
